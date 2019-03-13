e1d89b52c53efda0671a9fac37edbb1fe5e47e3e
Alexey Kalenkevich <alexei.kalenkevich@talenya.com>
2019-03-13 15:50:41 +0300

    draw lines with scale

--- index.js
+++ index.js
@@ -1,36 +1,73 @@
 const primaryChartColor = '#AAAAAA';
 const chartWidth = 600;
 const chartHeight = 500;
-const previewWidth = 600;
-const previewHeight = 200;
-const previewActiveAreaDefaultWidth = previewWidth / 4;
-const previewActiveAreaStretchBorderWidth = 5;
+const legendWidth = 600;
+const legendHeight = 100;
+const legendActiveAreaDefaultWidth = legendWidth / 4;
+const legendActiveAreaStretchBorderWidth = 5;
 
-class ChartGraphic {
-    constructor(element, data) {
-        this.data = data;
-        this.context = element.getContext("2d");
-    }
-
-    render() {
-        const lineIndex = 1;
-        const [name2, ...values2] = this.data.columns[lineIndex];
-
-        const strokeStyle = this.context.strokeStyle;
-        this.context.strokeStyle = this.data.colors[name2];
-
-        for (let index = 0; index < values2.length; index) {
-            const path = new Path2D();
-            path.moveTo(index * 5, values2[index]);
-            path.lineTo(++index * 5, values2[index]);
-
-            this.context.stroke(path);
-        }
-
-        this.context.strokeStyle = strokeStyle;
+class Utils {
+    static getMaxValueFromArray(array) {
+        return (array || []).reduce((maxVal, value) => maxVal > value ? maxVal : value);
     }
 }
 
+/**
+ * Class for showing graphics itself (lines)
+ */
+class ChartGraphic {
+    constructor(element, data) {
+        this.data = data;
+        this.element = element;
+        this.context = element.getContext("2d");
+    }
+
+    getMaxValueFromColumns(columns) {
+        return (columns || []).reduce((maxValue, column) => {
+            const [name, ...values] = column;
+
+            if (this.data.types[name] === "x") {
+                return;
+            }
+
+            const currentMaxValue = Utils.getMaxValueFromArray(values);
+
+            return maxValue > currentMaxValue ? maxValue : currentMaxValue;
+        }, 0);
+    }
+
+    render() {
+        const [, ...xAxis] = (this.data.columns || []).find(column => this.data.types[column[0]] === "x");
+        const scaleX = this.element.width / xAxis.length;
+        const scaleY = this.element.height / this.getMaxValueFromColumns(this.data.columns);
+
+        (this.data.columns || []).forEach((column, index) => {
+            const [name, ...values] = this.data.columns[index];
+
+            if (this.data.types[name] === "x") {
+                return;
+            }
+
+            const strokeStyle = this.context.strokeStyle;
+            this.context.strokeStyle = this.data.colors[name];
+
+            for (let index = 0; index < values.length;) {
+                const path = new Path2D();
+                path.moveTo(index * scaleX, values[index] * scaleY);
+                index++;
+                path.lineTo(index * scaleX, values[index] * scaleY);
+
+                this.context.stroke(path);
+            }
+
+            this.context.strokeStyle = strokeStyle;
+        });
+    }
+}
+
+/**
+ * Class which manage graphic and axis
+ */
 class Chart {
     constructor(element, data) {
         this.data = data;
@@ -43,16 +80,19 @@ class Chart {
     }
 }
 
-class PreviewActiveArea {
+/**
+ * Class which manage active view of legend of the Chart
+ */
+class ChartLegendActiveArea {
     constructor(element) {
         this.element = element;
         this.context = element.getContext('2d');
         this.dim = {
-            width: previewActiveAreaDefaultWidth,
-            height: previewHeight,
+            width: legendActiveAreaDefaultWidth,
+            height: legendHeight,
         };
         this.pos = {
-            x: previewWidth - previewActiveAreaDefaultWidth * 2,
+            x: legendWidth - legendActiveAreaDefaultWidth * 2,
             y: 0,
         };
         this.offset = {
@@ -75,17 +115,17 @@ class PreviewActiveArea {
         };
 
         const isLeftBorder = grabOffset.x >= 0
-            && grabOffset.x <= previewActiveAreaStretchBorderWidth
+            && grabOffset.x <= legendActiveAreaStretchBorderWidth
             && grabOffset.y >= 0
             && grabOffset.y <= this.dim.height;
 
         const isRightBorder = grabOffset.x <= this.dim.width
-            && this.dim.width - grabOffset.x <= previewActiveAreaStretchBorderWidth
+            && this.dim.width - grabOffset.x <= legendActiveAreaStretchBorderWidth
             && grabOffset.y >= 0
             && grabOffset.y <= this.dim.height;
 
-        const isPreviewArea = grabOffset.x >= previewActiveAreaStretchBorderWidth
-            && grabOffset.x <= this.dim.width - previewActiveAreaStretchBorderWidth
+        const isPreviewArea = grabOffset.x >= legendActiveAreaStretchBorderWidth
+            && grabOffset.x <= this.dim.width - legendActiveAreaStretchBorderWidth
             && grabOffset.y >= 0
             && grabOffset.y <= this.dim.height;
 
@@ -129,8 +169,8 @@ class PreviewActiveArea {
                     newPosX = 0;
                 }
 
-                if (newPosX > this.pos.x + this.dim.width - previewActiveAreaStretchBorderWidth * 3) {
-                    newPosX = this.dim.width + this.pos.x - previewActiveAreaStretchBorderWidth * 3;
+                if (newPosX > this.pos.x + this.dim.width - legendActiveAreaStretchBorderWidth * 3) {
+                    newPosX = this.dim.width + this.pos.x - legendActiveAreaStretchBorderWidth * 3;
                 }
 
                 if (newPosX + this.dim.width > this.element.width) {
@@ -151,8 +191,8 @@ class PreviewActiveArea {
             const onMouseMove = (event) => {
                 let newPosX = event.pageX - this.offset.left;
 
-                if (newPosX < this.pos.x + previewActiveAreaStretchBorderWidth * 3) {
-                    newPosX = this.pos.x + previewActiveAreaStretchBorderWidth * 3;
+                if (newPosX < this.pos.x + legendActiveAreaStretchBorderWidth * 3) {
+                    newPosX = this.pos.x + legendActiveAreaStretchBorderWidth * 3;
                 }
 
                 if (newPosX > this.element.width) {
@@ -195,23 +235,26 @@ class PreviewActiveArea {
         this.context.fillRect(
             this.pos.x,
             this.pos.y,
-            previewActiveAreaStretchBorderWidth,
+            legendActiveAreaStretchBorderWidth,
             this.dim.height,
         );
         this.context.fillRect(
-            this.pos.x + this.dim.width - previewActiveAreaStretchBorderWidth,
+            this.pos.x + this.dim.width - legendActiveAreaStretchBorderWidth,
             this.pos.y,
-            previewActiveAreaStretchBorderWidth,
+            legendActiveAreaStretchBorderWidth,
             this.dim.height,
         );
     }
 }
 
-class ChartPreview {
+/**
+ * Class which manage legend of the Chart
+ */
+class ChartLegend {
     constructor(element, data) {
         this.element = element;
         this.context = element.getContext("2d");
-        this.activeArea = new PreviewActiveArea(element);
+        this.activeArea = new ChartLegendActiveArea(element);
         this.backgroundChart = new ChartGraphic(element, data);
     }
 
@@ -244,6 +287,10 @@ class ChartPreview {
     }
 }
 
+
+/**
+ * Class which manage canvas elements
+ */
 class ChartWidget {
     constructor(id, data) {
         this.chartContainer = document.getElementById(id);
@@ -258,24 +305,25 @@ class ChartWidget {
         this.chart.height = chartHeight;
         this.chart.style.border = `1px solid ${primaryChartColor}`;
 
-        this.preview = document.createElement('canvas');
-        this.preview.id = 'preview';
-        this.preview.width = previewWidth;
-        this.preview.height = previewHeight;
-        this.preview.style.border = `1px solid ${primaryChartColor}`;
+        this.legend = document.createElement('canvas');
+        this.legend.id = 'legend';
+        this.legend.width = legendWidth;
+        this.legend.height = legendHeight;
+        this.legend.style.border = `1px solid ${primaryChartColor}`;
 
         this.chartContainer.appendChild(this.title);
         this.chartContainer.appendChild(this.chart);
-        this.chartContainer.appendChild(this.preview);
+        this.chartContainer.appendChild(this.legend);
 
         this.chart = new Chart(this.chart, this.data);
-        this.preview = new ChartPreview(this.preview, this.data);
+        this.legend = new ChartLegend(this.legend, this.data);
     }
+
     show() {
         setInterval(() => {
             //TODO here should be another mechanism of rendering!!!
-            this.preview.render();
-            this.chart.render(this.preview.activeArea);
+            this.legend.render();
+            this.chart.render(this.legend.activeArea);
         }, 25);
     }
 }
